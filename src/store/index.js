6c3e2234ec955cae2280bafae14bf0ed1175f51a
m7bellei <mateus_xdzin@hotmail.com>
2024-04-05 22:17:22 -0300

    configured store and vuerouter

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+// src/store/index.js
+
+import { createStore } from 'vuex'
+
+export default createStore({
+  state() {
+    return {
+      // Seus estados aqui
+    }
+  },
+  mutations: {
+    // Suas mutações aqui
+  },
+  actions: {
+    // Suas ações aqui
+  },
+  getters: {
+    // Seus getters aqui
+  }
+})
