ff7945d1a929d927f8cd0705120057d3f979ded0
m7bellei <mateus_xdzin@hotmail.com>
2024-04-05 22:59:07 -0300

    instaled css and finished app starter config

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import router from './router'
 import store from './store'
+import App from './App.vue' 
 
 createApp(App).use(router).use(store).mount('#app')
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,5 @@
 <template>
-  hello world
+  <div class="bg-black">
+    <p class="text-red-600 text-lg">hello world</p>
+  </div>
 </template>
